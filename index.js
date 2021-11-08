4744a2ea65f6a9e80f449556dc30914eb456683f
Zohaib <zohaibmanzoor1700@gmail.com>
2021-11-08 15:09:12 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//write your code here
+// Declare function names averageInArray, initialize with one parameter named arr
+const averageInArray = arr => {
+  // Initialize total variable to hold sum of numbers
+  let total = 0;
+  // Loop through everything in array 
+  // for (let i = 0; i < arr.length; i++) or 
+  for (let I of arr) {
+    // Push sum into total
+    total += I; {
+    }
+  } 
+  // return sum / numbers (length)  
+  return total / arr.length;
+};
+
+console.log(averageInArray([2, 2, 2, 2]));
